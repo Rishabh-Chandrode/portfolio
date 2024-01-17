13fe2a292900a9e07d28d1e150c402728d946033
rishabh <rishabhchandrode@gmail.com>
2024-01-17 23:49:12 +0530

    react added in navlink

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -7,7 +7,6 @@ const MenuOverlay = ({links}) => {
     <ul className='flex flex-col py-4 items-center'>
         {links.map( (link,index) => (
             <li key={index}>
-
             <NavLink href={link.path} title={link.title} />
             </li>
         ) )}
