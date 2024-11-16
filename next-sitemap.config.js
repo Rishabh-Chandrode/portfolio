ad4b17cd68e1716a2c653d3f81a65f1c90f8ba48
Rishabh-Chandrode <rishabhchandrode@gmail.com>
2024-11-16 22:47:53 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://portfolio-rishabh-chandrodes-projects.vercel.app/', 
+    generateRobotsTxt: true, 
+    sitemapSize: 5000, 
+    changefreq: 'daily', 
+    priority: 0.7,
+  };
+  
