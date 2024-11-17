5f655bdd83343a67a32ef11cd6cc64ba8cb2a261
Rishabh-Chandrode <rishabhchandrode@gmail.com>
2024-11-17 10:39:31 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
-        images: {
-            domains: ['drive.google.com'],
+    images: {
+      domains: ['drive.google.com'],
+    },
+  
+    async headers() {
+      return [
+        {
+          source: '/(.*)', // Apply to all routes
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'index, follow', // Allow indexing and crawling
+            },
+          ],
         },
-    
-}
-
-module.exports = nextConfig
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
