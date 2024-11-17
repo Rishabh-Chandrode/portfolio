49aedf66e4e3d4a39ce01135221fd8e819fd38b5
Rishabh-Chandrode <rishabhchandrode@gmail.com>
2024-11-17 10:41:42 +0530

    fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,19 +1,4 @@
 module.exports = {
-    async headers() {
-      return [
-        {
-          // Apply to all pages (all routes)
-          source: '/(.*)',  // This ensures all pages are included
-          headers: [
-            {
-              key: 'X-Robots-Tag',
-              value: 'index, follow', // Allow indexing and following links
-            },
-          ],
-        },
-      ];
-    },
-  
     // Other configurations if needed
     siteUrl: 'https://portfolio-rishabh-chandrodes-projects.vercel.app/',  // Your site URL for sitemap generation
     generateRobotsTxt: true, // Ensure robots.txt is generated
